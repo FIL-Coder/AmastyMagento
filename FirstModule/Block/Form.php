78a74309198ae6dfaeac2c6dd8b8b271ba7631c2
FIL-Coder <fil.coder@Gmail.com>
2021-07-19 12:24:24 +0300

    Task 2, without additional

---
+++ FirstModule/Block/Form.php
@@ -0,0 +1,6 @@
+<?php
+namespace Amasty\FirstModule\Block;
+class Form extends \Magento\Framework\View\Element\Template
+{
+
+}
--- FirstModule/Controller/Index/AmastyLove.php
+++ FirstModule/Controller/Index/AmastyLove.php
@@ -7,8 +7,8 @@ class AmastyLove extends \Magento\Framework\App\Action\Action
 
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\View\Result\PageFactory $pageFactory)
-    {
+        \Magento\Framework\View\Result\PageFactory $pageFactory
+    ) {
         $this->_pageFactory = $pageFactory;
         return parent::__construct($context);
     }
