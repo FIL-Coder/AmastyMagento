fda1014336c872a276d0c404a1b6608275246d25
FIL-Coder <fil.coder@Gmail.com>
2021-07-23 11:56:51 +0300

    Task 3 done, without additional

--- FirstModule/Block/Form.php
+++ FirstModule/Block/Form.php
@@ -2,7 +2,31 @@
 
 namespace Amasty\FirstModule\Block;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\View\Element\Template\Context;
+
 class Form extends \Magento\Framework\View\Element\Template
 {
+    /**
+     * @var ScopeConfigInterface
+     */
+    protected $scopeConfig;
 
+    public function __construct(
+        Context $context,
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+        parent::__construct($context);
+    }
+
+    public function onOffQtyField()
+    {
+        return $this->scopeConfig->isSetFlag('first_config/general/show_qty');
+    }
+
+    public function getDefaultQty()
+    {
+        return $this->scopeConfig->getValue('first_config/general/default_qty');
+    }
 }
--- FirstModule/Block/Hello.php
+++ FirstModule/Block/Hello.php
@@ -2,15 +2,26 @@
 
 namespace Amasty\FirstModule\Block;
 
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
 class Hello extends \Magento\Framework\View\Element\Template
 {
-    public function __construct(\Magento\Framework\View\Element\Template\Context $context)
-    {
+    /**
+     * @var ScopeConfigInterface
+     */
+    protected $scopeConfig;
+
+    public function __construct(
+        Context $context,
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
         parent::__construct($context);
     }
 
-    public function sayHello()
+    public function getGreetingText()
     {
-        return __('Hello Amasty. I am your student now. ');
+        return $this->scopeConfig->getValue('first_config/general/greeting_text');
     }
 }
