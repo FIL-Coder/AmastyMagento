e58710423876176cff75e2076e904cf68ee4d0f4
FIL-Coder <fil.coder@Gmail.com>
2021-07-26 23:59:31 +0300

    task 4 without additional

--- FirstModule/Block/Form.php
+++ FirstModule/Block/Form.php
@@ -20,7 +20,7 @@ class Form extends \Magento\Framework\View\Element\Template
         parent::__construct($context);
     }
 
-    public function onOffQtyField()
+    public function enabledQty()
     {
         return $this->scopeConfig->isSetFlag('first_config/general/show_qty');
     }
@@ -28,5 +28,10 @@ class Form extends \Magento\Framework\View\Element\Template
     public function getDefaultQty()
     {
         return $this->scopeConfig->getValue('first_config/general/default_qty');
+    }
+
+    public function getFormAction()
+    {
+        return $this->getUrl('firstmodule/index/addproduct');
     }
 }
